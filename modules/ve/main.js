3a12f2771c6f1328abc249cfbe2b7cd2f34ad27e
alex4401 <alex@wiki.gg>
2025-07-14 17:29:17 +0200

    refactor!: drop old VE code
    Unused and hugely out of date; new VE will be made from scratch anyway after core updates.

--- modules/ve/main.js
+++
@@ -1,8 +0,0 @@
-const MapVisualEditor = require( './editor.js' ),
-    { MapFlags } = require( 'ext.datamaps.core' );
-
-mw.dataMaps.onMapInitialised( map => {
-    if ( map.checkFeatureFlag( MapFlags.VisualEditor ) ) {
-        map.on( 'legendManager', () => ( map.ve = new MapVisualEditor( map ) ) );
-    }
-}, null, mw.dataMaps.IS_COMPATIBLE_WITH_VISUAL_EDITOR );
