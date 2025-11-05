d53d3581d81a56894dbab4241c88d7c13289dc76
alex4401 <alex@wiki.gg>
2025-11-05 05:44:17 +0100

    MapViewer: Provide a readiness callback in the ViewportManager

--- modules/ext.navigator.map.app/viewport/LeafletViewportManager.js
+++ modules/ext.navigator.map.app/viewport/LeafletViewportManager.js
@@ -5,16 +5,24 @@ const
 module.exports = class LeafletViewportManager {
     #mountTargetElement;
     #map;
+    #readinessPromise;
+    #readinessPromiseResolve;
 
 
     constructor( mountTargetElement ) {
         this.#mountTargetElement = mountTargetElement;
         this.#map = null;
+        this.#readinessPromise = new Promise( resolve => ( this.#readinessPromiseResolve = resolve ) );
     }
 
 
     getLeafletHandle() {
         return this.#map;
+    }
+
+
+    async waitUntilReady() {
+        return this.#readinessPromise;
     }
 
 
@@ -33,5 +41,7 @@ module.exports = class LeafletViewportManager {
         this.#map.addLayer( new Leaflet.Rectangle(
             [ [ 25, 25 ], [ 75, 75 ] ],
             { color: '#00f', weight: 1 } ) );
+        
+        this.#readinessPromiseResolve();
     }
 };
