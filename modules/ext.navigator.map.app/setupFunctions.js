3e58e3951e997ff899daa655201b3873b29e170e
alex4401 <alex@wiki.gg>
2025-11-14 07:27:12 +0100

    MapViewer: Load the configuration data when initialising the embed

--- modules/ext.navigator.map.app/setupFunctions.js
+++ modules/ext.navigator.map.app/setupFunctions.js
@@ -1,10 +1,30 @@
 const
-    MapEmbed = require( './MapEmbed.js' );
+    MapEmbed = require( './MapEmbed.js' ),
+    apiClient = new mw.Api();
 
 
-function initialiseEmbed( mountTargetElement ) {
+async function fetchMapConfig( pageId, revId ) {
+    return await apiClient.get( {
+        action: 'getmap',
+        pageid: pageId,
+        revid: revId,
+        prop: [ 'config' ],
+    } );
+}
+
+
+async function initialiseEmbed( mountTargetElement ) {
+    // Unserialise the embed config
+    const initConfig = JSON.parse( mountTargetElement.getAttribute( 'data-mw-navigator' ) );
+    // TODO: check the metadata version /again/
+
+    // Construct the viewer without the viewport, and reserve screen space to indicate progress
     mountTargetElement.classList.add( 'ext-navi-map--ready' );
     const embed = new MapEmbed( mountTargetElement );
+
+    const liveConfig = ( await fetchMapConfig( initConfig.pageId, initConfig.revId ) ).map;
+
+    // Enable the viewport now that the core setup is done
     embed.getViewportManager().enable();
     return embed;
 }
