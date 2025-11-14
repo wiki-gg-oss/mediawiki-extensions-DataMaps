bc20c2472bdf66e9aedde297544d0160b1b5e232
alex4401 <alex@wiki.gg>
2025-11-14 08:22:33 +0100

    Revert "Api: Generate virtual IDs for marker type groups"
    This reverts commit 4a081ae1c763e39bb838799da74311326b57f76b.

--- includes/API/ApiMapPropConfig.php
+++ includes/API/ApiMapPropConfig.php
@@ -5,8 +5,6 @@ namespace MediaWiki\Extension\DataMaps\Api;
 use stdClass;
 
 class ApiMapPropConfig extends ApiMapPropBase {
-	private int $anonMarkerTypeId = 0;
-
 	public function __construct( $query, $moduleName ) {
 		parent::__construct( $query, $moduleName, 'c' );
 	}
@@ -59,8 +57,6 @@ class ApiMapPropConfig extends ApiMapPropBase {
 			$result['id'] = $data->id;
 		} else {
 			$result['type'] = 'Group';
-			$idCounter = ++$this->anonMarkerTypeId;
-			$result['id'] = "dyn+{$idCounter}";
 		}
 
 		$result['name'] = $data->name;
