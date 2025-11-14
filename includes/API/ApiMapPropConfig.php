ca990160747afca4d8b741d16607043a6eb5180d
alex4401 <alex@wiki.gg>
2025-11-14 01:23:00 +0100

    Api: Export marker types in the config property

--- includes/API/ApiMapPropConfig.php
+++ includes/API/ApiMapPropConfig.php
@@ -2,9 +2,11 @@
 
 namespace MediaWiki\Extension\DataMaps\Api;
 
+use stdClass;
+
 class ApiMapPropConfig extends ApiMapPropBase {
 	public function __construct( $query, $moduleName ) {
-		parent::__construct( $query, $moduleName, 'mc' );
+		parent::__construct( $query, $moduleName, 'c' );
 	}
 
 	protected function getAllowedParams() {
@@ -12,7 +14,56 @@ class ApiMapPropConfig extends ApiMapPropBase {
 	}
 
 	public function execute() {
-		$result = $this->getResult();
-		$result->addValue( 'map', 'test', 'Hello world' );
+		$this->getParent()->fetchContent();
+		$this->outputSettings();
+		$this->outputMarkerTypes();
+	}
+
+	private function outputSettings(): void {
+		$data = $this->getParent()->fetchContent()->getData()->getValue();
+
+		$settings = $data->settings ?? new stdClass();
+		$this->getResult()->addValue( 'map', 'displayCoordinates', $settings->displayCoordinates ?? true );
+	}
+
+	private function outputMarkerTypes(): void {
+		$this->getResult()->addValue( 'map', 'markerTypes', $this->transcribeMarkerTypes() );
+	}
+
+	private function transcribeMarkerTypes(): array {
+		$data = $this->getParent()->fetchContent()->getData()->getValue();
+		if ( !isset( $data->markerTypes ) ) {
+			return [];
+		}
+
+		return $this->transformMarkerTypeArray( $data->markerTypes );
+	}
+
+	private function transformMarkerTypeArray( array $items ): array {
+		return array_map( fn ( $item ) => $this->transformMarkerType( $item ), $items );
+	}
+
+	private function transformMarkerType( stdClass $data ): array {
+		$parser = $this->getWikitextParser();
+		$result = [];
+
+		if ( isset( $data->id ) ) {
+			$result['type'] = 'MarkerType';
+			$result['id'] = $data->id;
+		} else {
+			$result['type'] = 'Group';
+		}
+
+		$result['name'] = $parser->parse( $data->name, stripOuterParagraph: true );
+
+		if ( isset( $data->description ) ) {
+			$result['description'] = $parser->parse( $data->description );
+		}
+
+		if ( isset( $data->include ) ) {
+			$result['subTypes'] = $this->transformMarkerTypeArray( $data->include );
+		}
+
+		return $result;
 	}
 }
