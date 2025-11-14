4b0f20a3641eb6903d29b29de93ac45210514dad
alex4401 <alex@wiki.gg>
2025-11-14 01:20:42 +0100

    Validation: Clean up marker type structure to allow a type to also be a group

--- includes/Content/Validation/Entities/PolymorphicMarkerTypeValidator.php
+++ includes/Content/Validation/Entities/PolymorphicMarkerTypeValidator.php
@@ -8,18 +8,24 @@ class PolymorphicMarkerTypeValidator extends EntityValidator {
      * @inheritDoc
      */
     public function validateObject( stdClass $data ): bool {
-        if ( $this->hasAnyProperty( $data, [ 'group', 'include' ] ) ) {
-            $this->expectProperties( $data, [
-                'group' => [ 'is_string' ],
-                'description' => [ 'is_string', EntityValidator::NULLABLE => true ],
-                'include' => [ 'is_array', EntityValidator::ITEM_SPEC => [ 'is_object',
+        $common = [
+            'name' => [ 'is_string' ],
+            'description' => [ 'is_string', EntityValidator::NULLABLE => true ],
+            'include' => [ 'is_array', EntityValidator::NULLABLE => true,
+                EntityValidator::ITEM_SPEC => [ 'is_object',
                     EntityValidator::CHECK_CLASS => PolymorphicMarkerTypeValidator::class ] ],
-            ] );
-        } else {
+        ];
+
+        if ( $this->hasAnyProperty( $data, [ 'id' ] ) ) {
+            // This is a usable marker type that can be used for display or grouping
             $this->expectProperties( $data, [
                 'id' => [ 'is_string' ],
-                'name' => [ 'is_string' ],
-                'description' => [ 'is_string', EntityValidator::NULLABLE => true ],
+                ...$common,
+            ] );
+        } else {
+            // This is only a marker type grouping that cannot be used for markers
+            $this->expectProperties( $data, [
+                ...$common,
             ] );
         }
 
