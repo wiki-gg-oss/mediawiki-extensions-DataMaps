1578cb3b14dc78f21c6f6e99b8491c9bbbf73314
sizau <hi@sizau.com>
2025-12-21 20:58:56 +0800

    i18n: add Chinese translations for namespaces

--- i18n/Namespaces.php
+++ i18n/Namespaces.php
@@ -10,3 +10,13 @@ $namespaceNames['en'] = [
     2900 => 'Map',
     2901 => 'Map_talk',
 ];
+
+$namespaceNames['zh-hans'] = [
+    2900 => '地图',
+    2901 => '地图讨论',
+];
+
+$namespaceNames['zh-hant'] = [
+    2900 => '地圖',
+    2901 => '地圖討論',
+];
